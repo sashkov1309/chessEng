5ac18a5b9bf6b0b832e983e4593a6f2e69c0e668
robot_native_ssd <robot@getsomatic.com>
2022-04-18 20:44:55 +0300

    pieces set

--- main.cpp
+++ main.cpp
@@ -33,9 +33,33 @@ board.Draw();*/
 
 int main() {
     ChessBoard board(8, 8);
-    board.AddPiece(std::make_shared <Pawn>(Position(1, 0), true, true));
-    board.AddPiece(std::make_shared <Pawn>(Position(1, 1), true, true));
+    /// BLACK
+    for(int i = 0; i < 8; i++) {
+        board.AddPiece(std::make_shared <Pawn>(Position(1, i), false, true));
+    }
+    board.AddPiece(std::make_shared <Rook>(Position(0, 0), false));
+    board.AddPiece(std::make_shared <Rook>(Position(0, 7), false));
+    board.AddPiece(std::make_shared <Bishop>(Position(0, 2), false));
+    board.AddPiece(std::make_shared <Bishop>(Position(0, 5), false));
+    board.AddPiece(std::make_shared <Knight>(Position(0, 1), false));
+    board.AddPiece(std::make_shared <Knight>(Position(0, 6), false));
+    board.AddPiece(std::make_shared <King>(Position(0, 4), false));
+    board.AddPiece(std::make_shared <Queen>(Position(0, 3), false));
+
+    /// WHITE
+    for(int i = 0; i < 8; i++) {
+        board.AddPiece(std::make_shared <Pawn>(Position(6, i), true, true));
+    }
+    board.AddPiece(std::make_shared <Rook>(Position(7, 0), false));
+    board.AddPiece(std::make_shared <Rook>(Position(7, 7), false));
+    board.AddPiece(std::make_shared <Bishop>(Position(7, 2), false));
+    board.AddPiece(std::make_shared <Bishop>(Position(7, 5), false));
+    board.AddPiece(std::make_shared <Knight>(Position(7, 1), false));
+    board.AddPiece(std::make_shared <Knight>(Position(7, 6), false));
+    board.AddPiece(std::make_shared <King>(Position(7, 4), false));
+    board.AddPiece(std::make_shared <Queen>(Position(7, 3), false));
     board.Draw();
+
     return 0;
 }
 
